c430e62849b0a9b29ca90cb3d9becf8fb56dcd9e
kento sasamoto <knote0721@gmail.com>
2016-10-19 18:33:29 +0900

    操作方法をver1にする

--- src/main.js
+++ src/main.js
@@ -2,19 +2,18 @@ import React from 'react'
 import ReactCSSTransitionGroup from 'react/lib/ReactCSSTransitionGroup'
 import ReactDOM from 'react-dom'
 import json from '../data.json' //初期データ
-
 let data = json.data
 //keyを割り振る
 for(let i = 0; i < data.length; i += 1) {
-  if (i == 0) { data.unshift({"title": "トップ"}) }
+  // if (i == 0) { data.unshift({"title": "トップ"}) }
   data[i].key = i
   if (!data[i].children) { data[i].children = [] }
   for(let j = 0; j < data[i].children.length; j += 1) {
-    if (j == 0) { data[i].children.unshift({"title": "戻る"}) }
+    // if (j == 0) { data[i].children.unshift({"title": "戻る"}) }
     data[i].children[j].key = i+"-"+j
     if (!data[i].children[j].children) { data[i].children[j].children = [] }
     for( let k = 0; k < data[i].children[j].children.length; k +=1) {
-      if (k == 0) { data[i].children[j].children.unshift({"title": "戻る"}) }
+      // if (k == 0) { data[i].children[j].children.unshift({"title": "戻る"}) }
       data[i].children[j].children[k].key = i + "-" + j + "-" + k
       if (!data[i].children[j].children[k].children) { data[i].children[j].children[k].children = [] }
     }
@@ -25,6 +24,8 @@ for(let i = 0; i < data.length; i += 1) {
 let pressTimer
 //longPressか判断する変数
 let longPress
+//keyDownの初回だけメソッド呼び出すために
+let keyPressed = false
 //戻るのタイマー入れとくとこ
 let backTimer
 //ブラウジング中のURL
@@ -36,9 +37,46 @@ let Main = React.createClass({
     return {pos: pos}
   },
   posBack: function() {
-    console.log("時間経過")
     let pos = this.state.pos
-    backTimer = setTimeout(this.posBack, 1500)
+    backTimer = setTimeout(this.posBack, 1000)
+    // 現在位置のitemを取ってくる
+    let list = data;
+    for(let i = 0; i < pos.length-1; i += 1) {
+      list = list[pos[i]].children
+    }
+    let item = list[pos[pos.length-1]]
+    if (longPress || pos.length == 1) {　// LongPress or 最上階層であれば移動しない
+    } else { // Listの一番上なので戻る要素だから上の階層に移動
+      pos.pop()
+      this.setState({pos: pos})
+    }
+  },
+  keyPress: function(e) {
+    if(!keyPressed) {
+      keyPressed = true
+      longPress = false
+      pressTimer = setTimeout(this.pressTimer, 200)
+    }
+    clearTimeout(backTimer)
+  },
+  keyUp: function(e) {
+    keyPressed = false
+    clearTimeout(pressTimer)
+    backTimer = setTimeout(this.posBack, 1000)
+    let pos = this.state.pos
+    if(!longPress) {
+      // 現在位置のListを取ってくる
+      let list = data;
+      for(let i = 0; i < pos.length-1; i += 1) {
+        list = list[pos[i]].children
+      }
+      pos[pos.length-1] = (list.length-1 === pos[pos.length-1] ? 0 : pos[pos.length-1] + 1)
+    }
+    this.setState({pos: pos})
+  },
+  pressTimer: function() {
+    let pos = this.state.pos
+    longPress = true
     // 現在位置のitemを取ってくる
     let list = data;
     for(let i = 0; i < pos.length-1; i += 1) {
@@ -47,45 +85,11 @@ let Main = React.createClass({
     let item = list[pos[pos.length-1]]
     // urlを持っていれば遷移
     if (item.url) {
-      if (browsing != item.url) {
-        console.log("コンテンツ遷移")
-        browsing = item.url
-        window.opener.location.href = item.url
-      }
-
-    // 最上階層であれば移動しない
-    } else if (pos[pos.length-1] == 0 && pos.length == 1) {
-
-    // Listの一番上なので戻る要素だから上の階層に移動
-    } else if (pos[pos.length-1] == 0) {
-      pos.pop()
-      pos[pos.length-1] = 0
-      this.setState({pos: pos})
-
+      browsing = item.url
+      window.opener.location.href = item.url
     // 下の階層に移動
     } else {
-      pos.push(1)
-      this.setState({pos: pos})
-    }
-  },
-  keyDown: function(e) {
-    longPress = false
-    // pressTimer = setTimeout(this.pressTimer, 200)
-    clearTimeout(backTimer)
-  },
-  keyUp: function(e) {
-    // clearTimeout(pressTimer)
-    backTimer = setTimeout(this.posBack, 2000)
-    let pos = this.state.pos
-    console.log(pos)
-    if(!longPress) {
-      console.log("shortPress")
-      // 現在位置のListを取ってくる
-      let list = data;
-      for(let i = 0; i < pos.length-1; i += 1) {
-        list = list[pos[i]].children
-      }
-      pos[pos.length-1] = (list.length-1 === pos[pos.length-1] ? 0 : pos[pos.length-1] + 1)
+      pos.push(0)
     }
     this.setState({pos: pos})
   },
@@ -96,7 +100,7 @@ let Main = React.createClass({
     return (
       <div>
         <Items items={data} pos={this.state.pos.join('-')}/>
-        <input type="text" ref="nameInput" onKeyPress={this.keyDown} onKeyUp={this.keyUp}/>
+        <input type="text" ref="nameInput" onKeyPress={this.keyPress} onKeyUp={this.keyUp}/>
       </div>
     );
   }
